3b0f00b15a93fbf5d7fe0accee802dc0faa94513
LEE DONGEUN <tetares4@gmail.com>
2017-12-20 17:08:51 +0900

    [CLIENT] 各行動のテストを作成

---
+++ unity/Assets/Scripts/EnemyBehaviorController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+using AI.Player;
+
+namespace AI.Enemy
+{
+    using Behavior = EnemyBehaviorModel.Behavior;
+
+    public class EnemyBehaviorController
+    {
+        // TODO: 依存性がやばい。PlayerModel、EnemyModelを直接じゃなく何か必要
+        public Behavior GetBehavior(PlayerModel player, EnemyModel enemy)
+        {
+            if (Vector3.Distance(player.Pos, enemy.Pos) > enemy.SearchLength)
+                return Behavior.StandBy;
+
+            switch (enemy.Tendency)
+            {
+                case EnemyModel.TENDENCY.BLAVE:
+                    return Behavior.Attack;
+                case EnemyModel.TENDENCY.TIMID:
+                    return Behavior.Escape;
+                case EnemyModel.TENDENCY.VULGAR:
+                    bool power = player.Power * EnemyBehaviorModel.POWER_CRITERION < enemy.Power;
+                    bool hp = player.Hp * EnemyBehaviorModel.HP_CRITERION < enemy.Hp;
+                    Behavior state = power && hp ? Behavior.Attack : Behavior.Escape;
+                    return state;
+                default:
+                    return Behavior.StandBy;
+            }
+        }
+    }
+}
--- unity/Assets/Scripts/PlayerModel.cs
+++ unity/Assets/Scripts/PlayerModel.cs
@@ -1,8 +1,19 @@
-﻿public class PlayerModel {
-    public int Power;
+﻿using UnityEngine;
 
-    public PlayerModel(int power)
+namespace AI.Player
+{
+    public class PlayerModel
     {
-        Power = power;
+        public int Power { get; set; }
+        public int Speed { get; set; }
+        public int Hp { get; set; }
+        public Vector3 Pos { get; set; }
+
+        public PlayerModel(int power, int speed, int hp)
+        {
+            Power = power;
+            Speed = speed;
+            Hp = hp;
+        }
     }
 }
