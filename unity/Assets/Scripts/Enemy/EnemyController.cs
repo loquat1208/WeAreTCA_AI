e6c0234e56af79d0be66e9d363a3e2bbdcfec9fa
LEE DONGEUN <tetares4@gmail.com>
2018-01-14 18:17:20 +0900

    [CLIENT] Playerを追い付くAIを実装

--- unity/Assets/Scripts/Enemy/EnemyController.cs
+++ unity/Assets/Scripts/Enemy/EnemyController.cs
@@ -1,4 +1,7 @@
 ﻿using UnityEngine;
+using UnityEngine.AI;
+
+using System.Collections;
 
 namespace AI.Unit.Enemy
 {
@@ -6,9 +9,32 @@ namespace AI.Unit.Enemy
     {
         public EnemyModel Model { get; set; }
 
+        private Transform player;
+        private NavMeshAgent nav;
+
         private void Start()
         {
             Model = new EnemyModel();
+
+            player = GameObject.FindGameObjectWithTag("Player").transform;
+
+            nav = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update()
+        {
+            if (nav.enabled)
+                nav.SetDestination(player.position);
+        }
+
+        public void OnChase()
+        {
+            nav.isStopped = false;
+        }
+
+        public void OnStay()
+        {
+            nav.isStopped = true;
         }
     }
 }
