1ac44df531301102d672688b1d6cbc082ae564cc
LEE DONGEUN <tetares4@gmail.com>
2018-01-17 19:33:30 +0900

    [CLIENT] Playerの攻撃アニメーション追加

--- unity/Assets/Scripts/Player/PlayerController.cs
+++ unity/Assets/Scripts/Player/PlayerController.cs
@@ -15,26 +15,30 @@ namespace AI.Unit.Player
 
         public PlayerModel Model { get; set; }
 
-        private Rigidbody rigid { get { return GetComponent<Rigidbody>(); } }
+        private bool isAttack = false;
         private Animator anim { get { return GetComponent<Animator>(); } }
 
         void Start()
         {
             Model = new PlayerModel();
-            view.OnDirKey.Subscribe(dir =>
-            {
-                if (dir != Vector3.zero)
-                    OnMove(dir);
-                else
-                    OnStay();
-            }).AddTo(this);
+
+            view.OnDirKey
+                .Where(_ => !isAttack)
+                .Subscribe(dir =>
+                {
+                    if (dir != Vector3.zero)
+                        OnMove(dir);
+                    else
+                        OnStay();
+                }).AddTo(this);
+
             view.OnAttackKey.Subscribe(_ => OnAttack()).AddTo(this);
         }
 
         private void OnMove(Vector3 dir)
         {
-            rigid.velocity = dir * Model.Speed;
-            rigid.rotation = Quaternion.LookRotation(dir);
+            transform.position += dir * Model.Speed * Time.deltaTime;
+            transform.localRotation = Quaternion.LookRotation(dir);
             anim.SetBool("param_idletorunning", true);
         }
 
@@ -45,13 +49,26 @@ namespace AI.Unit.Player
 
         private void OnAttack()
         {
+            anim.SetTrigger("param_idletohit01");
+        }
+
+        // NOTO: AttackStart,EndはAnimationCommponentで処理
+        private void AttackStart()
+        {
+            isAttack = true;
             attackTrigger.Target
                 .ForEach(x =>
                 {
                     EnemyController enemy = x.GetComponent<EnemyController>();
                     if (enemy != null)
                         enemy.Model.Hp -= Model.Power;
+                    Debug.Log(enemy.Model.Hp);
                 });
+        }
+
+        private void AttackEnd()
+        {
+            isAttack = false;
         }
     }
 }
