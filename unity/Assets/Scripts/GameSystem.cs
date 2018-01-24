1d0a18685c3c9aa236f3d37b77f0090266989321
LEE DONGEUN <tetares4@gmail.com>
2018-01-24 21:12:51 +0900

    [CLIENT] マルチディスプレイ実装

---
+++ unity/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace AI.System
+{
+    public class GameSystem : MonoBehaviour
+    {
+
+        void Start()
+        {
+            if (Display.displays.Length > 1)
+                Display.displays[1].Activate();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
