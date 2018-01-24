db83b42ca405e5009416f2a423cdc8e1ff1ec6c5
tcagame <tcagame1>
2018-01-24 10:52:22 +0900

    [Server] ＬｉｔＪｓｏｎのインストール 学校のＰＣにＵｎｉｔｙ2017が無い分、実行ができない

---
+++ unity/Assets/Server/ServerConnect.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ServerConnect : MonoBehaviour {
+	
+	string railsServer = "http://localhost:3000/";
+
+	public void GetJsonToDebug( string url, string enemyNum = null, UnityAction callback = null ) { 
+		var debugPrintStr = "url: " + url + "\n" + "type: GET";
+		Debug.logger.Log(debugPrintStr);
+		StartCoroutine(GetJSON(url, callback ));
+	}
+
+	public IEnumerator GetJSON(string url, UnityAction callback){
+		var urlStr = railsServer + url;
+		var www = new WWW(urlStr);
+		yield return www;
+		if(!string.IsNullOrEmpty(www.error)){
+			Debug.logger.Log ("エラー発生");
+		} else {
+			var date = LitJson.JsonMapper.Toobject(www.text);
+			callback(date);
+		}
+
+	}
+	
+}
